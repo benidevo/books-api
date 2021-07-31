1b11531e5a1d8e0db48bacf7b5d16607b083102a
BENJAMIN IDEWOR <benjamin.idewor@decagon.dev>
2021-07-31 15:13:07 +0100

    add CRUD implentation to Books API

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    year_published = models.CharField(max_length=10)
+    review = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GRAPHENE = {
-    'SCHEMA': 'api.schema.schema'
+GRAPHENE = {   
+    "SCHEMA": "api.schema.schema"
 }
